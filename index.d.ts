8a42a911d5225ed588c617cc00ba1d7c9b344935
Vytenis Urbonavicius <vytenisu@wix.com>
2024-08-02 13:34:51 +0300

    Fixed types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import {Generator} from 'npm-dts';
 
 declare module 'npm-dts-webpack-plugin' {
-  export class NpmDtsPlugin {
+  export default class NpmDtsPlugin {
     constructor(options: ConstructorParameters<typeof Generator>[0]);
   }
 }
